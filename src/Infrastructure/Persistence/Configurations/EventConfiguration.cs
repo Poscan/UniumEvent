62a2d9af96ff4d5e8c6c3f831ba0eebdaf6dc98f
Poscan <doodark13@gmail.com>
2022-06-02 18:58:55 +0500

    [+] Add event seed

--- src/Infrastructure/Persistence/Configurations/EventConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/EventConfiguration.cs
@@ -8,6 +8,16 @@ public class EventConfiguration : IEntityTypeConfiguration<Event>
 {
     public void Configure(EntityTypeBuilder<Event> builder)
     {
-        builder.ToTable(nameof(Event));
+        builder.ToTable(nameof(Event)).HasData(new List<Event>()
+        {
+            new Event()
+            {
+                Id = 1, 
+                Name = "Городская Академия Юниум", 
+                PageName = "GAU",
+                StartDate = new DateTime(2022, 6, 1),
+                EndDate = new DateTime(2022, 7, 1)
+            },
+        });
     }
 }
